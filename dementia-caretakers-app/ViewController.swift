0c31f13d044ef047f080c214724232e5d0edfced
mayjang <mayjang@students.cs.ubc.ca>
2021-04-10 22:02:59 -0700

    Initial Commit

---
+++ dementia-caretakers-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dementia-caretakers-app
+//
+//  Created by May Jang on 2021-04-10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
